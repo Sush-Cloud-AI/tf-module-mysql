26b50646213dc33803845c4e0b27194426bc6e5d
Sush <sushcloud12@gmail.com>
2023-11-09 00:28:21 +0000

    i

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tf-module-mysql
+	tf-module-redis repo will be created in git . All the terraform code to provision MYSQL will be hosted in this repository. This will be the backend module, which will be called from the root module "terraform-databases".
---
+++ mysql.tf
@@ -0,0 +1,36 @@
+# # create RDS instance
+
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 10
+  db_name              = "roboshop-${var.ENV}-mysql"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "admin1"
+  password             = "roboshop1"
+  parameter_group_name = aws_db_parameter_group.default.name
+  db_subnet_group_name = aws_db_subnet_group.mysql.name
+  skip_final_snapshot  = true
+}
+
+
+# # parmater group allows to pass mutliple parameters in one shot
+resource "aws_db_parameter_group" "default" {
+  name   = "roboshop-${var.ENV}-mysql-pg"
+  family = "mysql5.7"
+}
+
+# # Creates mysql subnet group
+resource "aws_db_subnet_group" "mysql" {
+  name       = "roboshop-${var.ENV}-mysql-subnet-group"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+
+  tags = {
+    Name = "roboshop-${var.ENV}-mysql-subnet-group"
+  }
+}
+
+
+
+
+
