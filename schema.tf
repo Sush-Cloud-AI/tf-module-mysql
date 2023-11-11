41ba2105150aca3233316c9f5570c511ec1a8593
Sush <sushcloud12@gmail.com>
2023-11-11 03:57:33 +0000

    i

--- schema.tf
+++ schema.tf
@@ -17,29 +17,29 @@
 # }
 
 # # # # Injecting the schema
-#     resource "null_resource" "schema" {
+    resource "null_resource" "schema" {
 
-#     depends_on = [aws_db_instance.mysql]  
-#     provisioner "local-exec" {
-#             command = "cd /tmp"
-#         }
-#     provisioner "local-exec" {
-#             command = "curl -s -L -o /tmp/mysql.zip 'https://github.com/stans-robot-project/mysql/archive/main.zip'"
-#         }
+    depends_on = [aws_db_instance.mysql]  
+    provisioner "local-exec" {
+            command = "cd /tmp"
+        }
+    provisioner "local-exec" {
+            command = "curl -s -L -o /tmp/mysql.zip 'https://github.com/stans-robot-project/mysql/archive/main.zip'"
+        }
 
-#     provisioner "local-exec" {
-#             command = "cd /tmp"
-#         }
-#     provisioner "local-exec" {
-#             command = "unzip mysql.zip"
-#         }        
-#     # provisioner "local-exec" {
-#     #         command = "cd /tmp/mysql-main"
-#     #     }
+    provisioner "local-exec" {
+            command = "cd /tmp"
+        }
+    provisioner "local-exec" {
+            command = "unzip mysql.zip"
+        }        
+    # provisioner "local-exec" {
+    #         command = "cd /tmp/mysql-main"
+    #     }
 
-#     # provisioner "local-exec" {
-#     #         command = "mysql -h ${aws_db_instance.mysql.address} -uadmin1 -proboShop1 < shipping.sql"
-#     #     }
-#     }
+    # provisioner "local-exec" {
+    #         command = "mysql -h ${aws_db_instance.mysql.address} -uadmin1 -proboShop1 < shipping.sql"
+    #     }
+    }
 
 
