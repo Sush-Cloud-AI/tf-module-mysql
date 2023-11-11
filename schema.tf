a88f79b85b9ef47520420d826db0bc569d7ace79
Sush <sushcloud12@gmail.com>
2023-11-11 04:29:04 +0000

    i

--- schema.tf
+++ schema.tf
@@ -22,10 +22,10 @@
     depends_on = [aws_db_instance.mysql]  
     provisioner "local-exec" {
             command = <<EOF
-            cd
-            curl -s -L -o /root/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+                cd
+                curl -s -L -o /root/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
               
-              EOF
+            EOF
             }
         }
 
