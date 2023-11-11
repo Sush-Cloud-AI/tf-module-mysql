82dbc2bc44489746819e545248818231fe362729
Sush <sushcloud12@gmail.com>
2023-11-11 13:41:51 +0000

    i

--- schema.tf
+++ schema.tf
@@ -16,16 +16,16 @@
 
 # }
 
-# # # # Injecting the schema
-    resource "null_resource" "schema" {
+# # # # # Injecting the schema
+#     resource "null_resource" "schema" {
 
-    depends_on = [aws_db_instance.mysql]  
-    provisioner "local-exec" {
-            command = <<EOF
-                cd
-                curl -s -L -o /root/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+#     depends_on = [aws_db_instance.mysql]  
+#     provisioner "local-exec" {
+#             command = <<EOF
+#                 cd
+#                 curl -s -L -o /root/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
               
-            EOF
-            }
-        }
+#             EOF
+#             }
+#         }
 
