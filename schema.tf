eb81b18a01dad10a6193c45633bc4e4be98089a8
Sush <sushcloud12@gmail.com>
2023-11-11 02:11:19 +0000

    i

--- schema.tf
+++ schema.tf
@@ -17,17 +17,13 @@
 # }
 
 # Injecting the schema
-resource "null_resource" "schema" {
+    resource "null_resource" "schema" {
 
-  depends_on = [aws_db_instance.mysql]  
-  provisioner "local-exec" {
-         command = <<EOF
-            cd /tmp 
-            curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-            unzip -o /tmp/mysql.zip 
-            cd mysql-main
-            mysql -h ${aws_db_instance.mysql.address} -uadmin1 -proboshop1 < shipping.sql
-            
-        EOF 
+    depends_on = [aws_db_instance.mysql]  
+    provisioner "local-exec" {
+            command = <<EOF
+                cd /tmp 
+
+            EOF 
+        }
     }
-}
