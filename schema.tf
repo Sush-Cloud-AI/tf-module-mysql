31ff459fcf79be71e5a90e2bc081b88bd59c9210
Sush <sushcloud12@gmail.com>
2023-11-11 03:36:40 +0000

    i

--- schema.tf
+++ schema.tf
@@ -17,40 +17,25 @@
 # }
 
 # # Injecting the schema
-#     resource "null_resource" "schema" {
+    resource "null_resource" "schema" {
 
-#     depends_on = [aws_db_instance.mysql]  
-#     provisioner "local-exec" {
-#             command = "cd /tmp"
-#         }
-#     provisioner "local-exec" {
-#             command = "curl -s -L -o /tmp/mysql.zip 'https://github.com/stans-robot-project/mysql/archive/main.zip'"
-#         }
-#     provisioner "local-exec" {
-#             command = "unzip -o /tmp/mysql.zip"
-#         }
-#     provisioner "local-exec" {
-#             command = "cd /tmp/mysql-main"
-#         }
+    depends_on = [aws_db_instance.mysql]  
+    provisioner "local-exec" {
+            command = "cd /tmp"
+        }
+    provisioner "local-exec" {
+            command = "curl -s -L -o /tmp/mysql.zip 'https://github.com/stans-robot-project/mysql/archive/main.zip'"
+        }
+    # provisioner "local-exec" {
+    #         command = "unzip -o /tmp/mysql.zip"
+    #     }
+    # provisioner "local-exec" {
+    #         command = "cd /tmp/mysql-main"
+    #     }
 
-#     provisioner "local-exec" {
-#             command = "mysql -h ${aws_db_instance.mysql.address} -uadmin1 -proboShop1 < shipping.sql"
-#         }
-#     }
-
-# This is going to inject the schema to DocDB
-
-resource "null_resource" "mysql_schema" {
-       
-  depends_on = [aws_db_instance.mysql]
-
-  provisioner "local-exec" {
-         command = <<EOF
-            cd /tmp 
-            curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-            unzip -o /tmp/mysql.zip 
-            cd mysql-main
-            mysql -h ${aws_db_instance.mysql.address} -uadmin1 -proboShop1 < shipping.sql
-        EOF 
+    # provisioner "local-exec" {
+    #         command = "mysql -h ${aws_db_instance.mysql.address} -uadmin1 -proboShop1 < shipping.sql"
+    #     }
     }
-}
+
+
