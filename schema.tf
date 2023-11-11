0671ae91bc63b3d293bfaea5c0ba98167b4d008e
Sush <sushcloud12@gmail.com>
2023-11-11 14:01:03 +0000

    i

--- schema.tf
+++ schema.tf
@@ -23,7 +23,7 @@
     provisioner "local-exec" {
             command = <<EOF
                 cd /tmp
-                curl -s -L -o /root/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+                curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
               
             EOF
             }
